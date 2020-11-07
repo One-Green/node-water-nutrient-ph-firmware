731b1593d58266449f81bea95c07dc1113a22cfa
Vigneswaran <shanmugathas.vigneswaran@outlook.fr>
2020-11-07 15:18:12 +0100

    feat(esp32/esp32.cmdHandler): handle serial request 'GET_SENSORS'

--- mega-2560/mega-2560.ino
+++ mega-2560/mega-2560.ino
@@ -1,66 +1,59 @@
+/*
+ *  One-Green node type: water+nutrient+pH
+ *  Water supplier sprinkler nodes
+ *
+ *  Inputs / Outputs board, communicate with ESP32 by UART 0
+ *
+ *
+ * Author: Shanmugathas Vigneswaran
+ * email: shanmugathas.vigneswaran@outlook.fr
+ * */
+
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 #include "OGIO.h"
-
-byte greenLED = 12;
-char cString[20];
-byte chPos = 0;
-byte ch = 0;
-char dataStr[6];
 
 
 SoftwareSerial espSerial(2, 3); // RX, TX
 OGIO io_handler;
 
 void setup() {
-    Serial.begin(9600);
-    espSerial.begin(9600);
+    Serial.begin(115200);
+    espSerial.begin(115200);
     io_handler.initR();
 }
 
 void loop() {
+    cmdHandler();
 
-    int water_level_cm = io_handler.getWaterLevelCM();
-    int nutrient_level_cm = io_handler.getNutrientLevelCM();
-    int ph_downer_level_cm = io_handler.getPhDownerLevelCM();
-
-    float ph_level = io_handler.getPhLevel();
-    float tds_ppm = io_handler.getTDS();
-
-    Serial.print("Water level CM=");
-    Serial.println(water_level_cm);
-
-    Serial.print("Nutrient level CM=");
-    Serial.println(nutrient_level_cm);
-
-    Serial.print("ph Downer level CM=");
-    Serial.println(ph_downer_level_cm);
-
-    Serial.print("pH Level=");
-    Serial.println(ph_level);
-
-    Serial.print("TDS Level (ppm) =");
-    Serial.println(tds_ppm);
+}
 
 
-    while(espSerial.available())
-    {
-        //read incoming char by char:
-        ch = espSerial.read();
-        cString[chPos] = ch;
-        chPos++;
+void cmdHandler() {
 
-        digitalWrite(greenLED, HIGH); //flash led to show data is arriving
-        delay(10);
-        digitalWrite(greenLED, LOW);
+
+    String espSerialCmd;
+    StaticJsonDocument<200> doc;
+
+    if (Serial.available()) {
+        Serial.println("[espSerial] Command received");
+        espSerialCmd = Serial.readString();
     }
-    cString[chPos] = 0; //terminate cString
-    chPos = 0;
 
-    Serial.print(cString);
+    if (espSerialCmd == "GET_SENSORS") {
+        Serial.println("[espSerial] cmd=GET_SENSORS received");
 
+        doc["waterLevelCM"] = io_handler.getWaterLevelCM();
+        doc["nutrientLevelCM"] = io_handler.getNutrientLevelCM();
+        doc["pHDownerLevelCM"] = io_handler.getPhDownerLevelCM();
+        doc["phLevel"] = io_handler.getPhLevel();
+        doc["TDSLevel"] = io_handler.getTDS();
 
-    delay(1000);
+        Serial.println("Generated JSON: ");
+        serializeJson(doc, Serial);
+
+    }
 }
 
 
