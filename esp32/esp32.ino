51c96e83aa2f4bd02f15434784778bced7b27a9b
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-11-12 20:08:16 +0100

    feat(esp32.ino): chang UART to I2C

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,14 +1,65 @@
+/*
+ *  One-Green node type: water+nutrient+pH
+ *  Water supplier sprinkler nodes
+ *  ESP32 board firmware
+ *  Purpose:
+ *      - Interact with mega-2560 board for Inputs/Outputs
+ *      - Interact with master trough MQTT
+ *      - Interact with ST7735 screen
+ *
+ * I2C wiring :
+ *
+ *     +-----+-----------+-------+
+ *     |     | Mega 2560 | ESP32 |
+ *     +=====+===========+=======+
+ *     | SDA |  20       |  21   |
+ *     | SCL |  21       |  22   |
+ *     +-----+-----------+-------+
+ *
+ * Author: Shanmugathas Vigneswaran
+ * email: shanmugathas.vigneswaran@outlook.fr
+ * */
+
 #include <Arduino.h>
-#include <HardwareSerial.h>
+#include <ArduinoJson.h>
+#include <Wire.h>
 
-HardwareSerial espSerial(0);
-
-void setup() {
+void setup(){
     Serial.begin(9600);
+    while(!Serial){} // Waiting for serial connection
 
+    Serial.println();
+    Serial.println("Start I2C scanner ...");
+    Serial.print("\r\n");
+    byte count = 0;
+
+    Wire.begin();
+    for (byte i = 8; i < 120; i++)
+    {
+        Wire.beginTransmission(i);
+        if (Wire.endTransmission() == 0)
+        {
+            Serial.print("Found I2C Device: ");
+            Serial.print(" (0x");
+            Serial.print(i, HEX);
+            Serial.println(")");
+            count++;
+            delay(1);
+        }
+    }
+    Serial.print("\r\n");
+    Serial.println("Finish I2C scanner");
+    Serial.print("Found ");
+    Serial.print(count, HEX);
+    Serial.println(" Device(s).");
 }
+
 
 void loop() {
 
-}
+    String strJSON;
+    StaticJsonDocument<200> doc;
+    Serial.println("[espSerial] Sending cmd=\"GET_SENSORS\"");
+    delay(1000);
 
+}
