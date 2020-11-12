4e41f15e2084795085364108563092c2af6c2b94
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-11-13 00:27:50 +0100

    feat(mega-2560.ino): upgrade

--- mega-2560/mega-2560.ino
+++ mega-2560/mega-2560.ino
@@ -15,44 +15,48 @@
 #include <Thread.h>
 #include "OGIO.h"
 
+char GET_SENSORS[] = "GET_SENSORS";
+
 int waterLevelCM = 0;
 int nutrientLevelCM = 0;
 int pHDownerLevelCM = 0;
 int phLevel = 0;
 int TDSLevel = 0;
-String cmd = ""; // handle command received by ESP32 trough Wire
-StaticJsonDocument<200> doc;
 
+String cmd = ""; // handle command received by ESP32 trough Wire
+StaticJsonDocument<200> sensorsDict;
 OGIO io_handler;
 
 void setup() {
     Wire.begin(4);                  // join i2c bus with address #4
     Wire.onReceive(receiveEvent);   // register event
+    Wire.onRequest(requestEvent);   //
     Serial.begin(9600);             // start serial for output
-    Serial.println("slave setup");  //
+    Serial.println("[Wire] Slave started");  // print slave has started
     io_handler.initR();             // I/O handler
-    updatedSensors();               // update sensors values for first time
 }
 
 
-void updatedSensors() {
+void getSensors() {
     /*
-     *   Create doc JSON key values based
+     *   Read sensors values and
+     *   create doc JSON key values based
      *
      * */
     Serial.println("[Sensors] Updating sensors values ");
-    doc["waterLevelCM"] = io_handler.getWaterLevelCM();
-    doc["nutrientLevelCM"] = io_handler.getNutrientLevelCM();
-    doc["pHDownerLevelCM"] = io_handler.getPhDownerLevelCM();
-    doc["phLevel"] = io_handler.getPhLevel();
-    doc["TDSLevel"] = io_handler.getTDS();
+    sensorsDict["waterLevelCM"] = io_handler.getWaterLevelCM();
+    sensorsDict["nutrientLevelCM"] = io_handler.getNutrientLevelCM();
+    sensorsDict["pHDownerLevelCM"] = io_handler.getPhDownerLevelCM();
+    sensorsDict["phLevel"] = io_handler.getPhLevel();
+    sensorsDict["TDSLevel"] = io_handler.getTDS();
+    serializeJson(sensorsDict, Wire);
     Serial.println("[Sensors] Generated JSON: ");
-    serializeJson(doc, Serial);
+    serializeJson(sensorsDict, Serial);
     Serial.println();
 }
 
 void loop() {
-    //updatedSensors();
+    delay(100);
 }
 
 void receiveEvent(int howMany) {
@@ -68,4 +72,17 @@ void receiveEvent(int howMany) {
     }
     Serial.print("[Wire] Received command : ");
     Serial.println(cmd);
+
+    if (cmd == GET_SENSORS) {
+        Serial.println("[Wire] Request sensors values detected ...");
+    }
 }
+
+
+void requestEvent() {
+//
+//    if (cmd == GET_SENSORS) {
+//        getSensors();
+//    }
+}
+
