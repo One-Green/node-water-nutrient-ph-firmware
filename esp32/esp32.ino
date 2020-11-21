00ade5eca927b593a9a01b6bcdb08e3c536202cf
Shanmugathas Vigneswaran <vigneswaran.shanmugathas@nokia.com>
2020-11-21 16:41:22 +0100

    revert(esp32.ino): I2C to UART

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -7,87 +7,82 @@
  *      - Interact with master trough MQTT
  *      - Interact with ST7735 screen
  *
- * I2C wiring :
- *
- *     +-----+-----------+-------+
- *     |     | Mega 2560 | ESP32 |
- *     +=====+===========+=======+
- *     | SDA |  20       |  21   |
- *     | SCL |  21       |  22   |
- *     +-----+-----------+-------+
+ * ----------- ESP32 UART ----------------
+ * UART	    RX IO	TX IO	CTS	    RTS
+ * UART0	GPIO3	GPIO1	N/A	    N/A
+ * UART1	GPIO9	GPIO10	GPIO6	GPIO11
+ * UART2	GPIO16	GPIO17	GPIO8	GPIO7
+ * source : https://circuits4you.com/2018/12/31/esp32-hardware-serial2-example/
+ * ----------------------------------------
+ * Wiring :
+ *  ESP32           Mega
+ *  GPIO16 (RX) ->  D11 (TX)
+ *  GPIO17 (TX) ->  D10 (RX)
  *
  * Author: Shanmugathas Vigneswaran
  * email: shanmugathas.vigneswaran@outlook.fr
  * */
 
-#include <Arduino.h>
-#include <Wire.h>
+#include <ArduinoJson.h>
 
-int SLAVE_ADDRESS = 4;
-enum {
-    CMD_READ_ID = 1,
-    CMD_READ_WATER_LEVEL = 2,
-    CMD_READ_NUTRIENT_LEVEL = 3,
-    CMD_READ_PH_DOWNER_LEVEL = 4,
-    CMD_READ_PH_LEVEL = 5,
-    CMD_READ_TDS_LEVEL = 6,
-};
+#define RXD2 16
+#define TXD2 17
 
-void sendCommand(const byte cmd, const int responseSize) {
-    Wire.beginTransmission(SLAVE_ADDRESS);
-    Wire.write(cmd);
-    Wire.endTransmission();
-    Wire.requestFrom(SLAVE_ADDRESS, responseSize);
-}
+StaticJsonDocument<200> doc;
+char buffer[10];                        // Serial2/EXSerial buffer
+
+// ------------------------------------ // Exchange command ESP32-Mega
+char CMD_ALIVE[2] = "A";
+char CMD_READ_IO[3] = "IO";
 
 void setup() {
-    Wire.begin();
     Serial.begin(9600);
-    sendCommand(CMD_READ_ID, 1);
-    if (Wire.available()) {
-        Serial.print("Slave is ID: ");
-        Serial.println(Wire.read(), DEC);
-    } else
-        Serial.println("No response to ID request");
+    Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+    Serial.println("Serial/Serial2 ok");
+}
 
+void waitMega() {
+    /*
+     *  Wait Mega response
+     * */
+    Serial.print("[Serial2] Waiting response ...");
+    while (Serial2.available() <= 0) {
+        delay(20);
+        Serial.print('.');
+    }
+    Serial.println("");
+}
+
+bool checkMegaAlive() {
+    /*
+     *  Check if mega board is responding
+     *  to Serial2 trigger
+     *  TODO Implement ESP > MEGA reset if serial disconnected
+     *
+     * */
+    Serial2.write(CMD_ALIVE, 2);
+    waitMega();
+    Serial2.readBytes(buffer, 10);
+    // Serial.println("[Serial2] alive=" + String(buffer));
+    if (String(buffer) == String(CMD_ALIVE)) {
+        return true;
+    } else
+        return false;
+}
+
+void readMegaIO() {
+    /*
+     *  Read sensors and actuators status
+     *  buffer > JSON handled by ArduinoJson
+     *
+     * */
+    Serial2.write(CMD_READ_IO, 3);
+    waitMega();
+    Serial.print("[Serial2] Datas received : ");
+    deserializeJson(doc, Serial2);
+    serializeJsonPretty(doc, Serial);
 }
 
 void loop() {
-    int val;
-    sendCommand(CMD_READ_WATER_LEVEL, 2);
-    val = Wire.read();
-    val <<= 8;
-    val |= Wire.read();
-    Serial.print("Water level : ");
-    Serial.println(val, DEC);
-
-    sendCommand(CMD_READ_NUTRIENT_LEVEL, 2);
-    val = Wire.read();
-    val <<= 8;
-    val |= Wire.read();
-    Serial.print("Nutrient level : ");
-    Serial.println(val, DEC);
-
-    sendCommand(CMD_READ_PH_DOWNER_LEVEL, 2);
-    val = Wire.read();
-    val <<= 8;
-    val |= Wire.read();
-    Serial.print("PH downer level : ");
-    Serial.println(val, DEC);
-
-    sendCommand(CMD_READ_PH_LEVEL, 2);
-    val = Wire.read();
-    val <<= 8;
-    val |= Wire.read();
-    Serial.print("PH level : ");
-    Serial.println(val, DEC);
-
-    sendCommand(CMD_READ_TDS_LEVEL, 2);
-    val = Wire.read();
-    val <<= 8;
-    val |= Wire.read();
-    Serial.print("TDS level : ");
-    Serial.println(val, DEC);
-
-    delay(500);
+    readMegaIO();
 }
