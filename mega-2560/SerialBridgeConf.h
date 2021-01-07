82d62f41dbfbb24deafde39fb6fc47cff46282c9
Marco <marcoantoniociau@hotmail.com>
2021-01-07 15:30:35 -0600

    atmega serial bridge port and pins defined

--- mega-2560/SerialBridgeConf.h
+++ mega-2560/SerialBridgeConf.h
@@ -1,16 +1,16 @@
 #ifndef  SERIAL_BRIDGE_CONF_H
 #define  SERIAL_BRIDGE_CONF_H
 #include <Arduino.h>
-
+#include "SoftwareSerialDebug.h"
 
 
 /* Serial Bridge Max Response Timeout Def */
 #define BRIDGE_RESP_TIMEOUT_MS 700
 
 /* Serial Bridge Port, Baud and Pins Def */
-#define BRIDGE_TX_PIN TX
-#define BRIDGE_RX_PIN RX
-#define BRIDGE_PORT Serial2
+#define BRIDGE_TX_PIN 11
+#define BRIDGE_RX_PIN 10
+#define BRIDGE_PORT debugPort
 #define BRIDGE_BAUD 9600
 
 #define DEBUG_PORT Serial//TODO: define DEBUG PORT
