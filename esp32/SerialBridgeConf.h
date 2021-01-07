deb06d2d311f342b280b70287b653c6f8e7e9bec
Marco <marcoantoniociau@hotmail.com>
2021-01-07 11:51:25 -0600

    SerialBridge all code added

---
+++ esp32/SerialBridgeConf.h
@@ -0,0 +1,23 @@
+#ifndef  SERIAL_BRIDGE_CONF_H
+#define  SERIAL_BRIDGE_CONF_H
+#include <Arduino.h>
+
+
+
+/* Serial Bridge Max Response Timeout Def */
+#define BRIDGE_RESP_TIMEOUT_MS 700
+
+/* Serial Bridge Port, Baud and Pins Def */
+#define BRIDGE_TX_PIN TX
+#define BRIDGE_RX_PIN RX
+#define BRIDGE_PORT Serial
+#define BRIDGE_BAUD 9600
+
+#define DEBUG_PORT Serial//TODO: define DEBUG PORT
+
+/* Communication Role Def*/
+
+//#define DEVICE_SLAVE //for slave device (sensor device)
+#define DEVICE_MASTER //for master device (gateway)
+
+#endif
